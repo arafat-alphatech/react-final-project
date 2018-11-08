d83ffda856cc65639b7aa6b4f16a7b6436dc5c77
Alpha Tech <you@example.com>
2018-11-08 15:07:54 +0700

    add card

--- src/Components/CardBooks.js
+++ src/Components/CardBooks.js
@@ -9,7 +9,7 @@ class CardBooks extends Component {
       <div className="col-sm-3 mt-5">
         <div class="card w-auto">
           <img className="card-img-top " src="https://jakmall.id/images/stores/11e6834321155980b3be002590f3b526/uploads/20170712/Tas%20Ransel%20Laptop%20Anti%20Theft%20dengan%20USB%20Charger%201.jpg?ts=1499835204&s=2f8114b8df34c401cb047023d862b239"
-          // style={{width: "300px", height:"225px"}} 
+          // style={{width: "300px", height:"225px"  }} 
           />
           <div className="card-body">
             <h5 className="card-title" />
